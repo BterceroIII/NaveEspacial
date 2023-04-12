d93bc412f8721954ccad57db17137af6643c439f
Byron Tercero <B1tercero.cruz@gmail.com>
2023-04-11 21:48:15 -0600

    Programs: add various methods to initialize the game

--- NaveGame/Program.cs
+++ NaveGame/Program.cs
@@ -1,10 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using NaveGame;
+using NaveGame.Models;
 using System.Drawing;
 
 
 
-Ventana ventana = new Ventana(150, 44, ConsoleColor.Black, new Point(5, 3), new Point(145, 40));
-ventana.DibujarMarco();
+Ventana ventana;
+Nave nave;
+bool jugar = true;
+
+
+// Iniciamos por un metodo todos los componentes
+void Iniciar()
+{
+    Ventana ventana = new Ventana(150, 44, ConsoleColor.Black, new Point(5, 3), new Point(145, 40));
+    ventana.DibujarMarco();
+    nave = new Nave(new Point(72,30), ConsoleColor.White,ventana);
+    nave.Dibujar();
+}
+
+void Game()
+{
+    while (jugar)
+    {
+        nave.Mover(2);
+    }
+}
+
+
+
+Iniciar();
+Game();
 Console.ReadKey();
