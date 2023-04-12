1fed1f1b562b69beb4abd1a893bc088bf1373b4b
Byron Tercero <B1tercero.cruz@gmail.com>
2023-04-11 23:48:11 -0600

    Nave: add all ship functionalities

--- NaveGame/Models/Nave.cs
+++ NaveGame/Models/Nave.cs
@@ -14,6 +14,10 @@ namespace NaveGame.Models
         public ConsoleColor Color { get; set; }
         public Ventana VentanaC { get; set; }
         public List<Point> PosicionesNave { get; set; }
+        public List<Bala> Balas { get; set; }
+        public float SobreCarga { get; set; }
+        public bool SobreCargaCondicion { get; set; }
+        public float BalaEspecial { get; set; }
 
         public Nave( Point posicion, ConsoleColor color, Ventana ventana )
         {
@@ -22,6 +26,7 @@ namespace NaveGame.Models
             Color = color;   
             VentanaC = ventana;
             PosicionesNave = new List<Point>();
+            Balas = new List<Bala>();
         }
 
         // Dibuja la nave en la consola
@@ -94,8 +99,72 @@ namespace NaveGame.Models
             distancia.X *= velocidad;
             distancia.Y *= velocidad;
             Posicion = new Point(Posicion.X + distancia.X, Posicion.Y + distancia.Y);
+
+            TecladoBala(tecla);
+
         }
 
+        public void TecladoBala(ConsoleKeyInfo tecla)
+        {
+            if (tecla.Key == ConsoleKey.RightArrow)
+            {
+                if (!SobreCargaCondicion)
+                {
+                    Bala bala = new Bala(new Point(Posicion.X + 6, Posicion.Y + 2),
+                    ConsoleColor.Yellow, TipoBala.Normal);
+
+                    Balas.Add(bala);
+                    SobreCarga += 1.2f;
+                    if (SobreCarga >= 100)
+                    {
+                        SobreCargaCondicion = true;
+                        SobreCarga = 100;
+                    }
+                }
+                
+
+            }
+            if (tecla.Key == ConsoleKey.LeftArrow)
+            {
+                if (!SobreCargaCondicion)
+                {
+                    Bala bala = new Bala(new Point(Posicion.X, Posicion.Y + 2),
+                    ConsoleColor.Yellow, TipoBala.Normal);
+
+                    Balas.Add(bala);
+                    SobreCarga += 1.2f;
+                    if (SobreCarga >= 100)
+                    {
+                        SobreCargaCondicion = true;
+                        SobreCarga = 100;
+                    }
+                }
+                
+            }
+            if (tecla.Key == ConsoleKey.UpArrow)
+            {
+                if (BalaEspecial >= 100)
+                {
+                    Bala bala = new Bala(new Point(Posicion.X + 2, Posicion.Y - 2),
+                        ConsoleColor.Red, TipoBala.Especial);
+                    Balas.Add(bala);
+                    BalaEspecial = 0;
+                }
+            }
+        }
+
+        public void Disparar()
+        {
+            for (int i = 0; i < Balas.Count; i++)
+            {
+                if (Balas[i].Mover(1, VentanaC.LimiteSuperior.Y))
+                {
+                    Balas.Remove(Balas[i]);
+                }
+            }
+        }
+
+        // para que la nave no se salga del marco de la consola
         public void Colisiones(Point distancia)
         {
            Point posicionAux = new Point(Posicion.X + distancia.X, Posicion.Y + distancia.Y);
@@ -107,9 +176,9 @@ namespace NaveGame.Models
             {
                 posicionAux.X = VentanaC.LimiteInferior.X - 7;
             }
-            if (posicionAux.Y <= VentanaC.LimiteSuperior.Y)
+            if (posicionAux.Y <= (VentanaC.LimiteSuperior.Y) + 15)
             {
-                posicionAux.Y = VentanaC.LimiteSuperior.Y + 1;
+                posicionAux.Y = (VentanaC.LimiteSuperior.Y + 1) +15;
             }
             if (posicionAux.Y + 2 >= VentanaC.LimiteInferior.Y)
             {
@@ -117,6 +186,52 @@ namespace NaveGame.Models
             }
 
             Posicion = posicionAux;
+        }
+
+        public void Informacion()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(VentanaC.LimiteSuperior.X, VentanaC.LimiteSuperior.Y - 1);
+            Console.Write($"VIDA: {(int)Vida} %");
+
+            if (SobreCarga <= 0)
+            {
+                SobreCarga = 0;
+            }
+            else
+            {
+                SobreCarga -= 0.0007f;
+            }
+
+            if (SobreCarga <= 50)
+            {
+                SobreCargaCondicion = false;
+            }
+
+            if (SobreCargaCondicion)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.SetCursorPosition(VentanaC.LimiteSuperior.X + 13, VentanaC.LimiteSuperior.Y - 1);
+            Console.Write($"SOBRECARGA: {(int)SobreCarga} %");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(VentanaC.LimiteSuperior.X + 32, VentanaC.LimiteSuperior.Y - 1);
+            Console.Write($"BALA ESPECIAL: {(int)BalaEspecial} %");
+            if (BalaEspecial >= 100)
+            {
+                BalaEspecial = 100;
+            }
+            else
+            {
+                BalaEspecial += 0.0077f;
+            }
+
         }
          
         // Metodo Mover que llama a otros metodos para su ejecucion
@@ -130,9 +245,25 @@ namespace NaveGame.Models
                 Colisiones(distancia);
                 Dibujar(); // Cada ves que se mueva la nave se dibuja de nuevo con la posicion actual
             }
+            Informacion();
         }
 
-
+        public void Muerte()
+        {
+            Console.ForegroundColor = Color;
+            foreach (Point item in PosicionesNave)
+            {
+                Console.SetCursorPosition(item.X, item.Y);
+                Console.Write("X");
+                Thread.Sleep(200);
+            }
+            foreach (Point item in PosicionesNave)
+            {
+                Console.SetCursorPosition(item.X, item.Y);
+                Console.Write(" ");
+                Thread.Sleep(200);
+            }
+        }
 
     }
 }
